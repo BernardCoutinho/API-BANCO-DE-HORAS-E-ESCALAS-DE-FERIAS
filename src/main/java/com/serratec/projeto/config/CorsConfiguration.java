a884825ad1e12f4403b293d17501cb80864d18ef
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2022-01-15 01:43:09 -0300

    ssss

--- src/main/java/com/serratec/projeto/config/CorsConfiguration.java
+++ src/main/java/com/serratec/projeto/config/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedHeaders("*").allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS", "HEAD",
+		registry.addMapping("/**").allowedHeaders("*").allowedMethods("GET", "DELETE", "PUT", "POST", "OPTIONS", "HEAD",
 				"TRACE", "CONNECT");
 	}
 }
