c6abc8df0bbaf1f9f6591f326debc161d41e0cce
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2022-01-15 16:16:06 -0300

    allowed origins

--- src/main/java/com/serratec/projeto/config/CorsConfiguration.java
+++ src/main/java/com/serratec/projeto/config/CorsConfiguration.java
@@ -10,6 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
+		.allowedOrigins("*")
 		.allowedHeaders("*")
 		.allowedMethods("GET", "DELETE", "PUT", "POST", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 	}
