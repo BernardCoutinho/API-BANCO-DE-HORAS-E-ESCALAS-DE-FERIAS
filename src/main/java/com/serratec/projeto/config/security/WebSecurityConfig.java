47b31c2a1d5fe5e5e202a5f132acba15b83c2978
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2022-01-17 01:16:45 -0300

    liberando apontamento na marra

--- src/main/java/com/serratec/projeto/config/security/WebSecurityConfig.java
+++ src/main/java/com/serratec/projeto/config/security/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			// -- swagger ui
 
 			"/v2/api-docs", "/webjars/**", "/v3/api-docs", "/swagger-resources/**", "/swagger-ui/**",
-			"/api/auth/signin", "/swagger-ui.html", "/equipes" };
+			"/api/auth/signin", "/swagger-ui.html", "/equipes", "/apontamento/**", "/apontamento/membros_equipe_folga_dia" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
